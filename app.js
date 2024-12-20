9c6742d5b6aa1aba98613cc2d9e0c6cd8ec12e94
BrianXYZ <brianchima22@gmail.com>
2024-12-20 12:19:21 +0100

    added grid creation logic using form input to generate grid of input size

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
+const container = document.querySelector(".container");
+const getNumberForm = document.querySelector(".getNumberForm");
 
-
+getNumberForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const { value } = getNumberForm.number;
+  const maxWidth = value * 50;
+  const totalGridSquare = value * value;
+  container.style.maxWidth = `${maxWidth}px`;
+  for (let i = 0; i < totalGridSquare; i++) {
+    const gridSquare = document.createElement("div");
+    gridSquare.classList.add("gridSquare");
+    container.append(gridSquare);
+  }
+});
